8e852ecd41e47b87369570937410fd74e392c156
Honcy Ye <yeholdon@gmail.com>
2023-02-17 10:18:55 +0800

    delete redundant files

--- grpctool.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-protoc --cpp_out=./ *.proto
-
-GRPC_CPP_PLUGIN=grpc_cpp_plugin
-GRPC_CPP_PLUGIN_PATH=`which ${GRPC_CPP_PLUGIN}`
-
-protoc --grpc_out=./ --plugin=protoc-gen-grpc=${GRPC_CPP_PLUGIN_PATH} *.proto 
-
